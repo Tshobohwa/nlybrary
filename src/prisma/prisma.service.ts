f39762c983d11871e976e2f582c936e0d392b388
Tshobohwa <stshobohwa@gmail.com>
2026-04-13 23:46:47 +0300

    Update code

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       adapter: new PrismaPg({
-        connectionString:
-          'postgresql://postgres:243243@localhost:5434/nlybrary?schema=public',
+        connectionString: config.get('DATABASE_URL'),
       }),
     });
   }
